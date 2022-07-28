0b62fc59724595fe01f11f3b2431f1ff126805b9
Sneha U <snehau259@gmail.com>
2022-07-28 08:56:57 +0530

    const created in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
 // ReactDOM.render(<App subject="Clarice" />, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App subject="Clarice" />
+    <App subject="Clarice" tasks={DATA} />
   </React.StrictMode>
 );
